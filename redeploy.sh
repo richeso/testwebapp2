63a5d8dc40b95f7f351b8d3f29758a85d695301d
AMU\vb023528 <richard_che-so@ch.honda.com>
2017-03-27 13:26:03 -0400

    Update to start webapp after deployment to tomcat

--- redeploy.sh
+++ redeploy.sh
@@ -10,3 +10,7 @@ docker rm mytomcat
 docker rm mysqldb
 docker rmi testwebapp2_db
 docker-compose up -d
+
+sleep 5
+## Start testwebapp
+curl -u tomcat:password http://mycoreos1:8080/manager/text/start?path=/testwebapp 
